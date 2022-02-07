de28dbc9e7a686e631719864fb28838ff2b847e0
AntonGorban <antogor.work@gmail.com>
2022-02-07 23:49:58 +0300

    messaging for DMs and chats

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { VK, API } from "vk-io";
+import { VK, API, MessageContext } from "vk-io";
 
 const myId = Number(process.env.MY_VK_ID);
 
@@ -16,21 +16,43 @@ const myId = Number(process.env.MY_VK_ID);
     vk.updates.on("message_new", async (context) => {
       const message = await api.messages.getById({ message_ids: context.id }).then((messages) => messages.items[0]);
       const user = await api.users.get({ user_ids: String(message.from_id) }).then((users) => users[0]);
-      // const conversationMembers = await api.messages.getConversationMembers({ peer_id: context.peerId });
-      // const conversations = await api.messages.getConversations({
-      //   peer_id: context.peerId,
-      //   start_message_id: context.id,
-      // });
+      const conversation = await api.messages
+        .getConversationsById({
+          peer_ids: context.peerId,
+        })
+        .then((conversation) => conversation.items[0]);
 
-      // debugger;
+      debugger;
       if (!isBotMessage(context.text || "", user.id))
-        await context.send(
-          templateString(
-            `ID: ${user.id}
+        switch (true) {
+          case conversationIsDM(context):
+            await context.send(
+              templateString(
+                `ID: ${user.id}
 ${user.first_name} ${user.last_name}`
-          ),
-          { dont_parse_links: true }
-        );
+              ),
+              { dont_parse_links: true }
+            );
+
+            break;
+
+          case conversationIsChat(context):
+            await context.send(
+              templateString(
+                `conversation ID: ${conversation.peer.id}
+conversation title: ${conversation.chat_settings.title}
+conversation photo ${conversation.chat_settings.photo.photo_200}
+ID: ${user.id}
+${user.first_name} ${user.last_name}`
+              ),
+              { dont_parse_links: true }
+            );
+
+            break;
+
+          default:
+            break;
+        }
     });
 
     await vk.updates.start();
@@ -48,7 +70,10 @@ https://github.com/AntonGorban/VKHater`;
 
 const isBotMessage = (message: string, userId: number) => userId === myId && message.match("From: VKHater");
 
-const xz = (context: any) => `
+const conversationIsDM = (context: MessageContext): boolean => context.isDM;
+const conversationIsChat = (context: MessageContext): boolean => context.isChat;
+
+const xz = (context: MessageContext) => `
 /* ---------------------------- Инфа по сообщения --------------------------- */
 id сообщения [id] : ${context.id},
 id сообщения беседы [conversationMessageId] : ${context.conversationMessageId},
